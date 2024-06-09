b2e8ad32037e56d85a77c35813447877b2d1f8b9
JhonAngulo <jhonmanuelm89@gmail.com>
2024-06-09 15:51:03 -0500

    add docker

--- app/main.py
+++ app/main.py
@@ -1,18 +1,23 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('data.csv')
+  '''
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
-
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
+  '''
 
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'South America']
+  countries = df['Country/Territory']
+  percentages =  df['World Population Percentage']
   charts.generate_pie_chart(countries, percentages)
-  
-  country = input('Type Country => ')
 
+  data = read_csv.read_csv('data.csv')
+  country = input('Type Country => ')
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
---
+++ web-server/main.py
@@ -0,0 +1,32 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1,2,3]
+
+@app.get("/contact")
+def get_list():
+    return {'name': 'Jhon Angulo', 'phone': '123456789'}
+
+@app.get("/items/", response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+
+def run():
+    store.get_categories()
+
+if __name__ == '__main__':
+    run()
